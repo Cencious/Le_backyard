a68ddb0aa8c218c8f5da4ff8b6a897ac2de8fccf
Cencious <user.email>
2022-06-20 07:49:15 +0300

    add the location class model

--- neighbour/models.py
+++ neighbour/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+#location model
+class location(models.Model):
+    name= models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save_location(self):
+        self.save()
+    
+    def __str__(self):
+        return self.name
+
+
