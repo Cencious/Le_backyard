8fc4f7d76349eb4cadf1674719e94054e02be8a0
Cencious <user.email>
2022-06-20 17:01:24 +0300

    change the db to postgresql

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'insta',
+        'NAME': 'yard',
         'USER': 'kakan',
         'PASSWORD':'Abiathar2022'
     }
@@ -121,7 +121,7 @@ cloudinary.config(
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
