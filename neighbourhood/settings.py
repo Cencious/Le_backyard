eb9bd631a3deaeeee1bb8082cf5a341c9d1c269b
Cencious <user.email>
2022-06-20 11:36:08 +0300

    postgresql database

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'neighbourhood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'insta',
+        'USER': 'kakan',
+        'PASSWORD':'Abiathar2022'
     }
 }
 
