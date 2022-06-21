4f9f7c0ee7d22079b4852b4b9faeda7cdce941bf
Cencious <user.email>
2022-06-21 12:56:19 +0300

    register post, neighborhood

--- neighbour/admin.py
+++ neighbour/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Location
+from .models import Profile,Location,NeighbourHood,Post,Business
 
 
 # Register your models here.
 admin.site.register(Location)
+admin.site.register(Business)
+admin.site.register(Profile)
+
+admin.site.register(NeighbourHood)
+admin.site.register(Post)
