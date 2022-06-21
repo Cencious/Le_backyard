15dcfe55c1224e83436b95d715b8a5eda37ef641
Cencious <user.email>
2022-06-21 12:58:21 +0300

    Add test case for Neighbourhood

--- neighbour/tests.py
+++ neighbour/tests.py
@@ -2,3 +2,36 @@ from django.test import TestCase
 
 # Create your tests here.
 
+from django.test import TestCase
+from .models import Post,  Profile, Location, NeighbourHood, Business
+from django.contrib.auth.models import User
+
+
+# Neighbourhood Model Tests
+class NeighbourhoodTestClass(TestCase):
+    def setUp(self):
+        # create a location instance
+        self.location = Location(name='Test Location')
+        self.location.save_location()
+        # create an admin user
+        self.admin = User.objects.create_superuser(
+            username='developer',
+            password='password'
+        )
+        self.neighbourhood = NeighbourHood(
+            name='Test Neighbourhood', location=self.location, occupants_count=100, admin_id=self.admin.id)
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.neighbourhood, NeighbourHood))
+
+    def test_save_method(self):
+        self.neighbourhood.create_neigborhood()
+        neighbourhoods = NeighbourHood.objects.all()
+        self.assertTrue(len(neighbourhoods) > 0)
+
+    def test_delete_method(self):
+        self.neighbourhood.create_neigborhood()
+        self.neighbourhood.delete()
+        neighbourhoods = NeighbourHood.objects.all()
+        self.assertTrue(len(neighbourhoods) == 0)
+
