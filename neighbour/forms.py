a85d1428d30e7c074f4c445d17b039b9b590583b
Cencious <user.email>
2022-06-21 12:56:56 +0300

    import models

--- neighbour/forms.py
+++ neighbour/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile,Post,Business,NeighbourHood
 
 
 class UserRegisterForm(UserCreationForm):
@@ -10,3 +11,25 @@ class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'prompt srch_explore'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password', 'class': 'prompt srch_explore'}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password', 'class': 'prompt srch_explore'}))
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'content','image')
+
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        fields = ('name', 'email','description','image')
+class NeighbourhoodForm(forms.ModelForm):
+    class Meta:
+        model = NeighbourHood
+        exclude = ('admin',)
+
+
+
