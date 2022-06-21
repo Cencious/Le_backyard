f1401da5125a8700e24830858e24f598724f9136
Cencious <user.email>
2022-06-21 12:59:03 +0300

    Add paths fr user pfofile joining hood search

--- neighbour/urls.py
+++ neighbour/urls.py
@@ -6,5 +6,11 @@ urlpatterns=[
     path('',views.home, name='home'),
     path('register/',views.register,name='register'), 
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('user/', views.profile, name='profile'),
+    path('search/', views.search_hood, name='search_hoods'),
+    
+    path('neighbourhood/',views.neighborhood,name='neighbourhood'),
+    path('joinhood/<neighborhood_id>',views.join_hood,name="joinhood"),
+    path('leavehood/<neighborhood_id>',views.leave_hood,name="leavehood")
 
 ]
