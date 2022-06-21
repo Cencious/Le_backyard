a54dc027c264990746bc1cc9865e0d7f57e9b367
Cencious <user.email>
2022-06-21 18:03:28 +0300

    add login url

--- neighbour/urls.py
+++ neighbour/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns=[
     path('',views.home, name='home'),
     path('register/',views.register,name='register'), 
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+   
     path('user/', views.profile, name='profile'),
     path('search/', views.search_hood, name='search_hoods'),
     
